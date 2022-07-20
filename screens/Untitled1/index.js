a044fd5fe9478213024b1b17b18b870cbef8c97c
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-07-20 14:11:03 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -14,13 +14,13 @@ const Untitled1 = () => {
       <Slider thumbStyle={{
       height: 20,
       width: 20
-    }} thumbTintColor="#ffdc00" maximumValue={12} minimumValue={9} style={{
-      left: 35,
-      top: 60,
+    }} thumbTintColor="#ffdc00" maximumValue={15} minimumValue={13} style={{
+      left: 0,
+      top: 56,
       position: "absolute",
       width: 150,
       height: 40
-    }} value={11} step={15} minimumTrackTintColor="#e34f4f" maximumTrackTintColor="#2ac73a" disabled={true}></Slider><FlatList renderItem={() => <View style={{
+    }} value={17} step={18} minimumTrackTintColor="#e34f4f" maximumTrackTintColor="#2ac73a" disabled={true}></Slider><FlatList renderItem={() => <View style={{
       width: 92,
       height: 60,
       backgroundColor: '#FFFFFF'
@@ -36,13 +36,20 @@ const Untitled1 = () => {
       }}>Lorem ipsum… </Text></View>} ItemSeparatorComponent={() => <View style={{
       backgroundColor: '#000000',
       height: 1
-    }} />} data={[1, 2, 3]} keyExtractor={item => item.toString()} style={{
+    }} />} data={[1, 2, 3, 4, 5, 6, 7, 8, 9]} keyExtractor={item => item.toString()} style={{
       left: 47,
       top: 142,
       position: "absolute",
-      width: 110,
-      height: 140
-    }}></FlatList></View>;
+      width: 100,
+      height: 100
+    }} horizontal={false} ListHeaderComponent={() => <Text>My list header</Text>}></FlatList><Slider thumbStyle={{
+      height: 20,
+      width: 20
+    }} thumbTintColor="#0000FF" maximumValue={1} minimumValue={0} style={{
+      position: "absolute",
+      width: 150,
+      height: 40
+    }}></Slider></View>;
 };
 
 export default Untitled1;
